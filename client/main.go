1d16e72dc573e755c8f49e13402bdf58f820c5a2
Benjamín <benjamin.camblor@sansano.usm.cl>
2020-10-13 04:20:59 -0300

    client-logistics rough implementation

--- client/main.go
+++ client/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"container/list"
 	"google.golang.org/grpc"
 	"log"
 	"../proto"
 	"os"
 	"bufio"
 	"fmt"
+	"io"
+	"encoding/csv"
+	"strconv"
 )
 
 func main(){
@@ -18,43 +22,113 @@ func main(){
 
 	client := proto.NewAddServiceClient(conn)
 
+	/*
 	message := proto.Request{
 		A:2,
 		B:4,
 	}
+	*/
 
-
-	fmt.Println("press a to add, m to multiply")
+	
+	fmt.Println("press p for pymes, r for retail")
 
 	reader := bufio.NewReader(os.Stdin)
 	char, _, err := reader.ReadRune()
+	var tipo string
+
+	//lista de códigos de seguimiento
+	listaSeguimiento := list.New()
 
 	if err != nil {
 	fmt.Println(err)
 	}
-
 	
-
 	switch char {
-	case 'a':
-	fmt.Println("a Key Pressed")
-	response, err := client.Add(context.Background(), &message)
-	if err != nil{
-		log.Fatalf("error when calling Add: %s", err)
+	case 'p':
+	fmt.Println("p Key Pressed")
+	csvfile, err := os.Open("pymes.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
 	}
-	//log.Print(response.Result)
-	log.Printf("Response from server: %d", response.Result)
-	break
-	
-	case 'm':
-	fmt.Println("m Key Pressed")
-	response, err := client.Multiply(context.Background(), &message)
-	if err != nil{
-		log.Fatalf("error when calling Add: %s", err)
-	}
-	//log.Print(response.Result)
-	log.Printf("Response from server: %d", response.Result)
-	break
-	}
+	r := csv.NewReader(csvfile)
+	for{
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		if  record[5]=="1"{
+			tipo="prioritario"
+		}else{
+			tipo="normal"
+		}
+		valor, err := strconv.ParseInt(record[2], 10, 64)
+		if err != nil {
+			fmt.Printf("Error converting valor")
+		}
+
+		request := proto.ClientRequest{
+			Tipo:tipo,
+			NombreProducto: record[1],
+			Valor: valor,
+			Origen: record[3],
+			Destino: record[4],
+		}
+
+		response, err := client.Order(context.Background(), &request)
+		if err != nil{
+			log.Fatalf("error when calling Order: %s", err)
+		}
+		listaSeguimiento.PushBack(response.Seguimiento)
+	}
+	
+	break
+
+	case 'r':
+	fmt.Println("r Key Pressed")
+	csvfile, err := os.Open("retail.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	r := csv.NewReader(csvfile)
+	for{
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		valor, err := strconv.ParseInt(record[2], 10, 64)
+		if err != nil {
+			fmt.Printf("Error converting valor")
+		}
+
+		request := proto.ClientRequest{
+			Tipo:"retail",
+			NombreProducto: record[1],
+			Valor: valor,
+			Origen: record[3],
+			Destino: record[4],
+		}
+
+		response, err := client.Order(context.Background(), &request)
+		if err != nil{
+			log.Fatalf("error when calling Order: %s", err)
+		}
+		listaSeguimiento.PushBack(response.Seguimiento)
+	}
+	break
+	}
+	
+	
+
+}
+
+func pedirEstado(idSeguimiento string){
+		
 }
--- logistics/main.go
+++ logistics/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net"
 	"time"
-
+	"sync"
 	"../proto"
 	guuid "github.com/google/uuid"
 	"google.golang.org/grpc"
@@ -35,6 +35,10 @@ type paquete struct {
 	intentos     int
 	fechaEntrega time.Time
 }
+
+//mutex
+var mutexColas = &sync.Mutex{}
+var mutexRegistro = &sync.Mutex{}
 
 //crear las colas
 var retail = list.New()
@@ -88,7 +92,9 @@ func (s *server) Order(ctx context.Context, request *proto.ClientRequest) (*prot
 		destino:        request.GetDestino(),
 		seguimiento:    seguimiento,
 	}
+	mutexRegistro.Lock()
 	registroOrdenes.PushBack(orden)
+	mutexRegistro.Unlock()
 	paquete := paquete{
 		id:       id,
 		tipo:     request.GetTipo(),
@@ -99,15 +105,37 @@ func (s *server) Order(ctx context.Context, request *proto.ClientRequest) (*prot
 	}
 	switch request.GetTipo() {
 	case "retail":
+		mutexColas.Lock()
 		retail.PushBack(paquete)
+		mutexColas.Unlock()
 		break
 	case "prioritario":
+		mutexColas.Lock()
 		prioritario.PushBack(paquete)
+		mutexColas.Unlock()
 		break
 	case "normal":
+		mutexColas.Lock()
 		normal.PushBack(paquete)
+		mutexColas.Unlock()
 		break
 	}
 
 	return &proto.ResponseToClient{Seguimiento: seguimiento.String()}, nil
+}
+
+func (s *server) RequestEstado(ctx context.Context, request *proto.EstadoRequest) (*proto.ResponseToClient, error){
+	mutexRegistro.Lock()
+	codigo, err := guuid.Parse(request.GetSeguimiento())
+	if err != nil{
+		log.Fatalf("failed to parse uuid")
+	}
+	for e := registroOrdenes.Front(); e != nil; e = e.Next(){
+		if e.Value.(registroOrden).seguimiento==codigo{
+			mutexRegistro.Unlock()
+			return &proto.ResponseToClient{Seguimiento: e.Value.(registroOrden).tipo}, nil// retorna tipo por mientras, ya que el registro no lleva el estado de la orden segun el pdf wtf
+		}
+
+	}
+	return &proto.ResponseToClient{Seguimiento: "NOT FOUND"}, nil
 }
