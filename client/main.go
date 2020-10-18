f585c693cd11ff6ea3139c27afd7f77f2ed47e26
BucketHeaD85 <krizalidf@gmail.com>
2020-10-17 23:57:24 -0300

    Primera version Cliente

--- client/main.go
+++ client/main.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"../proto"
+	"time"
 	"os"
 	"bufio"
 	"fmt"
@@ -15,7 +16,15 @@ import (
 )
 
 func main(){
-	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+	
+	var segundos int
+	fmt.Println("Intervalo en segundos entre órdenes del cliente")
+	_, err := fmt.Scanf("%d", &segundos)
+	if err != nil{
+		fmt.Println(err)
+	}
+	
+	conn, err := grpc.Dial("10.6.40.247:50051", grpc.WithInsecure())
 	if err != nil{
 		log.Fatalf("failed to connect: %s", err)
 	}
@@ -30,7 +39,7 @@ func main(){
 	*/
 
 	
-	fmt.Println("press p for pymes, r for retail")
+	fmt.Println("P para pymes, R para retail")
 
 	reader := bufio.NewReader(os.Stdin)
 	char, _, err := reader.ReadRune()
@@ -46,12 +55,12 @@ func main(){
 	switch char {
 	case 'p':
 	fmt.Println("p Key Pressed")
-	csvfile, err := os.Open("pymes.csv")
+	csvfile, err := os.Open("../pymes.csv")
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
 	r := csv.NewReader(csvfile)
-	for{
+	for{	
 		record, err := r.Read()
 		if err == io.EOF {
 			break
@@ -60,36 +69,40 @@ func main(){
 			log.Fatal(err)
 		}
 
-		if  record[5]=="1"{
+		if  record[4]=="1"{
 			tipo="prioritario"
+			fmt.Printf("Enviado prioritario\n")
 		}else{
 			tipo="normal"
+			fmt.Printf("Enviado normal\n")
 		}
-		valor, err := strconv.ParseInt(record[2], 10, 64)
+		valor, err := strconv.ParseInt(record[1], 10, 64)
 		if err != nil {
 			fmt.Printf("Error converting valor")
 		}
 
 		request := proto.ClientRequest{
 			Tipo:tipo,
-			NombreProducto: record[1],
+			NombreProducto: record[0],
 			Valor: valor,
-			Origen: record[3],
-			Destino: record[4],
+			Origen: record[2],
+			Destino: record[3],
 		}
 
 		response, err := client.Order(context.Background(), &request)
+		fmt.Println("ID:",response.Seguimiento)	
 		if err != nil{
 			log.Fatalf("error when calling Order: %s", err)
 		}
 		listaSeguimiento.PushBack(response.Seguimiento)
+		time.Sleep(time.Duration(segundos)*time.Second)
 	}
 	
 	break
 
 	case 'r':
 	fmt.Println("r Key Pressed")
-	csvfile, err := os.Open("retail.csv")
+	csvfile, err := os.Open("../retail.csv")
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
@@ -103,24 +116,26 @@ func main(){
 			log.Fatal(err)
 		}
 
-		valor, err := strconv.ParseInt(record[2], 10, 64)
+		valor, err := strconv.ParseInt(record[1], 10, 64)
 		if err != nil {
 			fmt.Printf("Error converting valor")
 		}
 
 		request := proto.ClientRequest{
 			Tipo:"retail",
-			NombreProducto: record[1],
+			NombreProducto: record[0],
 			Valor: valor,
-			Origen: record[3],
-			Destino: record[4],
+			Origen: record[2],
+			Destino: record[3],
 		}
 
 		response, err := client.Order(context.Background(), &request)
 		if err != nil{
 			log.Fatalf("error when calling Order: %s", err)
 		}
+		fmt.Printf("Enviado: Retail\n")
 		listaSeguimiento.PushBack(response.Seguimiento)
+		time.Sleep(time.Duration(segundos)*time.Second)
 	}
 	break
 	}
